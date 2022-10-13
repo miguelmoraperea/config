358e0e6f7149ea04377a14754acdfb302c7eef22
Miguel Mora Perea <morapmiguel@gmail.com>
2022-10-13 00:03:30 -0700

    UPD - Deprecated use of vim.highlight.create

--- common/nvim/.config/nvim/lua/mmp/init.lua
+++ common/nvim/.config/nvim/lua/mmp/init.lua
@@ -216,9 +216,9 @@ vim.keymap.set("n", "<leader>dfu", ":lua require('dap-python').test_method()<CR>
 vim.keymap.set("n", "<leader>dca", ":lua require('dap-python').test_class()<CR>", { noremap = false })
 vim.keymap.set("n", "<leader>dse", ":lua require('dap-python').debug_selection()<CR>", { noremap = false })
 
-vim.highlight.create('DapBreakpoint', { ctermbg=0, guifg='#993939', guibg='#31353f' }, false)
-vim.highlight.create('DapLogPoint', { ctermbg=0, guifg='#61afef', guibg='#31353f' }, false)
-vim.highlight.create('DapStopped', { ctermbg=0, guifg='#98c379', guibg='#31353f' }, false)
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg='#993939', bg='#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { fg='#61afef', bg='#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { fg='#98c379', bg='#31353f' })
 
 vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
