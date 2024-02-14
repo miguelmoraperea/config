289d9c94f2d6e9633952ca33398cbb786ea86d75
Miguel Mora Perea <morapmiguel@gmail.com>
2024-02-14 10:26:13 -0800

    ADD - null-ls.lua

---
+++ common/nvim/.config/nvim/lua/mmp/null-ls.lua
@@ -0,0 +1,13 @@
+local null_ls = require("null-ls")
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.diagnostics.pycodestyle,
+        null_ls.builtins.formatting.blue,
+        null_ls.builtins.formatting.jq,
+        null_ls.builtins.formatting.usort,
+    },
+})
