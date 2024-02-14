6d7dab4a452c87713a0613c90ea915ce12beb1ac
Miguel Mora Perea <morapmiguel@gmail.com>
2024-02-14 10:18:49 -0800

    ADD - 'leetcode.nvim' plugin

--- common/nvim/.config/nvim/lua/plugins/extra.lua
+++ common/nvim/.config/nvim/lua/plugins/extra.lua
@@ -27,4 +27,23 @@ return {
             { "<Leader>t", "<Cmd>AerialToggle<CR>" },
         },
     },
+
+    {
+        "kawre/leetcode.nvim",
+        build = ":TSUpdate html",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim", -- required by telescope
+            "MunifTanjim/nui.nvim",
+
+            -- optional
+            -- "nvim-treesitter/nvim-treesitter",
+            -- "rcarriga/nvim-notify",
+            -- "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            -- configuration goes here
+            lang = "python3",
+        },
+    },
 }
