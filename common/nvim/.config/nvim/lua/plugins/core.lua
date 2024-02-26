1f9fa83c776008db5a122f2f1da4ac3dfe3ba784
Miguel Mora Perea <morapmiguel@gmail.com>
2024-02-26 15:09:40 -0800

    ADD - zen-mode.nvim and zenbones.nvim plugins

--- common/nvim/.config/nvim/lua/plugins/core.lua
+++ common/nvim/.config/nvim/lua/plugins/core.lua
@@ -6,6 +6,15 @@ return {
         end,
     },
 
+    { "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" } },
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
     {
         "nvim-telescope/telescope.nvim",
         dependencies = {
