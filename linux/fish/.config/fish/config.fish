549ca6b03b4d2d2ccea414da7d8075f0be7a4974
Miguel Mora Perea <morapmiguel@gmail.com>
2024-04-11 17:22:27 -0700

    ADD - 'sudo !!' function to fish config

--- linux/fish/.config/fish/config.fish
+++ linux/fish/.config/fish/config.fish
@@ -83,6 +83,15 @@ function fish_prompt
   echo -n -s " "
 end
 
+# sudo !!
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+        echo sudo $history[1]
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
+end
 
 
 # Perforce Aliases
